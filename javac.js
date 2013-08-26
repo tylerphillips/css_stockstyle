a9aa978ebfb7ac10aeed9191ada2abe0a13df42c
Tyler Phillips <tylerphillips@Tylers-MacBook-Air.local>
2013-08-26 00:56:36 -0400

    Aug 25 update

---
+++ javac.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+function overlay() {
+	el = document.getElementById("overlay");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+};
